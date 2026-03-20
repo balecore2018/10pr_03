29cf749146b4404bed479b4791bbb7e9807ddb15
dima <balecored@gmail.com>
2026-03-20 21:45:11 +0500

    НА ТРОЙКУ КАК ОБЫЧНО СДЕЛАЛ))))))) Шаповалов

--- app/src/main/java/com/example/a10pr_03/MainActivity.java
+++ app/src/main/java/com/example/a10pr_03/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.a10pr_03;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,5 +34,32 @@ public class MainActivity extends AppCompatActivity {
                         });
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    EditText fio;
+    EditText phone;
+    EditText adress;
+
+    private boolean isPhoneValid(String phoneNumber) {
+        String phonePattern = "^\\+8 \\(9\\d{2}\\)-\\d{3}-\\d{2}-\\d{2}$";
+        return phoneNumber.matches(phonePattern);
+    }
+
+    public void OnArrange(View view){
+        fio = findViewById(R.id.etName);
+        phone = findViewById(R.id.etPhone);
+        adress = findViewById(R.id.etAddress);
+        String phoneText = phone.getText().toString().trim();
+
+        if(fio.getText().length() == 0) AlertDialog("!ВНИМАНИЕ!", "ВВЕДИ ФИО, НАМ НЕЧЕГО КРАСТЬ.!");
+        else if (phone.getText().length() == 0) AlertDialog("!ВНИМАНИЕ!", "ВВЕДИ НОМЕР, НАМ НЕЧЕГО КРАСТЬ.!");
+        else if (!isPhoneValid(phoneText)) {
+            AlertDialog("!ВНИМАНИЕ!", "ВВЕДИ НОМЕР В ФОРМАТЕ:\n+8 (912)-345-67-89\nНАМ НЕЧЕГО КРАСТЬ.!");
+        }
+        else if (adress.getText().length() == 0) AlertDialog("!ВНИМАНИЕ!", "ВВЕДИ АДРЕС, НАМ НЕЧЕГО КРАСТЬ.!");
+
+        else {
+            AlertDialog("Mission Complete", "Данные успешно украдены)");
+        }
     }
 }
